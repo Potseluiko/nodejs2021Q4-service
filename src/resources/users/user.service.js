01a6bb433443d2da48204457cc90d816919c8aca
Alexandra Ilyashenko <sasha.ilyashenko@onde.app>
2021-12-01 22:39:51 +0300

    feat: add user.memory.repository.js (database)

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,12 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getById = (id) => usersRepo.getById(id);
+
+const create = (data) => usersRepo.create(data);
+
+const updateById = (id, data) => usersRepo.updateById(id, data);
+
+const deleteById = (id) => usersRepo.deleteById(id);
+
+module.exports = { getAll, getById, create, updateById, deleteById };
